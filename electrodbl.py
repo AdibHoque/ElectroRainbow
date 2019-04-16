d0a8c00fdea43bc7fed139e74b5d08713c2de6be
AdibHoque <46087857+AdibHoque@users.noreply.github.com>
2019-04-16 14:06:18 +0600

    Update electrodbl.py

--- electrodbl.py
+++ electrodbl.py
@@ -12,7 +12,7 @@ from discord.ext import commands
 bot = commands.Bot 
 
 dbltoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxMDQ5MTI0MzE1NTgxNjQ0OSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ2NDExNzYxfQ.V_7sJcSceSDB93OR5ZaTkoGHQqQN2ic2uO7U8cSeQlM"
-url = "https://discordbots.org/api/bots/" + bot.user.id + "/stats"
+url = "https://discordbots.org/api/bots/510491243155816449/stats"
 headers = {"Authorization" : dbltoken}
 
 async def on_ready():
